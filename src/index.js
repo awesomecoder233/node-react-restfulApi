02c5a33549bcf35f3e60f0469546a33fe4dac0a7
awesomecoder233 <tommount58@gmail.com>
2021-07-23 03:28:34 +0200

    add okta login component in App.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Security } from '@okta/okta-react';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import * as serviceWorker from './servcieWorker';
+
+const oktaConfig = {
+  issuer: `${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`,
+  redirect_uri: `${window.location.origin}/login/callback`,
+  client_id: process.env.REACT_APP_OKTA_CLIENT_ID,
+}
+
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
+  <Security {...oktaConfig}>
     <App />
-  </React.StrictMode>,
+    </Security>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-reportWebVitals();
+
+if (module.hot) module.hot.accept();
+
